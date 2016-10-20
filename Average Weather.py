e9be52646030c383c63bf6df251eb7e887751a7c
eestey <eestey@students.mchenry.edu>
2016-10-20 05:32:52 -0500

    Update Average Weather.py

--- Average Weather.py
+++ Average Weather.py
@@ -1,26 +1,27 @@
 weather = {'January': '50', 'February': '52', 'March': '54', 'April': '57', 'May': '63', 'June': '70', 'July': '75', 'August': '75', 'September': '72', 'October': '64', 'November': '57', 'December': '54'}
-print weather
+
 
 # Variables
-avg_temp = 75
+average = 0
 high = dict()
 
 
-def avg_temp(month):
+def avg(month):
+    global average 
     total = 0
     for temp in month:
-        total += month[temp]
-    avg = total / len(weather)
+        total += int(month[temp])
+    average = total / len(weather)
     print(" The average temperature for Monte Carlo this year was: " + str(avg))
 
 
 def over(today):
-    global top_range
+    global average
     for be in today:
-        if today[be] > top_range:
+        if today[be] > average:
             high[be] = (today[be])
     print high
 
 
-avg_temp(weather)
+avg(weather)
 over(weather)
